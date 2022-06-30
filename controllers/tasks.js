2b156f0a95705b4110c61d305558070dfcd0d8cc
erictau <erictau04@gmail.com>
2022-06-30 13:41:58 -0700

    Adds task index page and modifies task delete redirect

--- controllers/tasks.js
+++ controllers/tasks.js
@@ -1,18 +1,45 @@
 const JobApp = require('../models/jobapp');
 
 module.exports = {
+    index,
     create,
     delete: deleteTask,
     update
 }
 
 
+function index(req, res) {
+    // Create date 1 week from today's date to query for tasks that are due within 1 week.
+    let date = new Date();
+    let urgentDate = new Date(date.getFullYear(), date.getMonth(), date.getDate() + 7);
+    function convertDateString(date) {
+        date = new Date(date.toLocaleDateString('en-US', {timeZone: 'UTC'}))
+        return date.getFullYear() + '-'
+        + ('0' + (date.getMonth() + 1)).slice(-2) + '-'
+        + ('0' + date.getDate()).slice(-2)
+    }
+    // Only grabs the job apps that have tasks with due dates within 1 week.
+    JobApp.find({
+        'tasks.dueDate': {$lt: urgentDate}
+    })
+    .then((jobApps) => {
+        res.render('tasks/index', {
+            title: 'Upcoming Tasks',
+            jobApps,
+            dates: {
+                date,
+                urgentDate
+            },
+            convertDateString
+        })
+    })
+}
+
 function create(req, res) {
     JobApp.findById(req.params.id)
     .then(jobApp => {
         jobApp.tasks.push(req.body);
-        jobApp.tasks.sort((a, b) => b.dueDate - a.dueDate)
-        console.log(jobApp.tasks)
+        jobApp.tasks.sort((a, b) => a.dueDate - b.dueDate)
         jobApp.save()
         .then(() => res.redirect(`/jobapps/${req.params.id}`))
     })
@@ -23,7 +50,7 @@ function deleteTask(req, res) {
     .then(jobApp => {
         jobApp.tasks.remove(req.params.taskId)
         jobApp.save()
-        .then(() => res.redirect(`/jobapps/${jobApp._id}`))
+        .then(() => res.redirect('back'))
     })
 }
 
--- routes/tasks.js
+++ routes/tasks.js
@@ -3,7 +3,7 @@ var router = express.Router();
 const tasksCtrl = require('../controllers/tasks')
 const isLoggedIn = require('../config/auth');
 
-
+router.get('/tasks', isLoggedIn, tasksCtrl.index);
 router.post('/jobapps/:id/tasks', isLoggedIn, tasksCtrl.create);
 router.delete('/tasks/:taskId', isLoggedIn, tasksCtrl.delete);
 router.put('/tasks/:taskId', isLoggedIn, tasksCtrl.update);
