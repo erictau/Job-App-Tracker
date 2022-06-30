7aa305412c821920b47e2819baafbfd4133b136b
erictau <erictau04@gmail.com>
2022-06-30 09:55:19 -0700

    Updates database connection to MongoDB Atlas

--- config/database.js
+++ config/database.js
@@ -1,7 +1,8 @@
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb://localhost:27017/jobapps');
+mongoose.connect(process.env.DATABASE_URL);
 
 const db = mongoose.connection;
 
 db.on('connected', () => console.log(`Connected to MongoDB at ${db.host}: ${db.port}`));
+
